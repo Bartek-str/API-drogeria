a7f970210dcf2838a85c625ef3aa576a5a38868e
Bartek-str <bartlomiejstruzyk@gmail.com>
2020-06-29 18:56:31 +0200

    done main side

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
+import logo from './../../assets/lisc_szarosc.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
+import {faFacebookSquare, faInstagram, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => (
     <div className='Footer'>
@@ -8,7 +9,21 @@ const Footer = () => (
             <div className='header'>Social Media</div>
             <div className='iconContainer'>
                 <FontAwesomeIcon icon={faFacebookSquare} className='facebook' />
+                <FontAwesomeIcon icon={faInstagram} className='instagram' />
+                <FontAwesomeIcon icon={faLinkedin} className='linkedIn' />
             </div>
+        </div>
+        <div className='newsletter'>
+            <input value='adres e-mail' />
+            <button>Newsletter</button>
+        </div>
+        <div className='bottom'>
+            <div className='rules'>Regulamin</div>
+            <div className='private-policy'>Polityka prywatności</div>
+        </div>
+        <div className='copyright'>
+            <img src={logo} alt='logo' />
+            <div>&copy; 2020 API Group</div>
         </div>
     </div>
 )
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,16 @@
 import React from "react";
-import Logo from "./../../assets/logo_1.png";
+import Logo from "./../../assets/lisc_kolor.svg";
 import Burger from "./Burger";
 
 const Header = () => (
     <div className='Header'>
-        <img src={Logo} alt='logo' />
+        <div className='logo'>
+            <div className='api'>API</div>
+            <div className='container'>
+                <img src={Logo} alt='lisc'/>
+                <div className='drogeria'>drogeria</div>
+            </div>
+        </div>
         <Burger />
     </div>
 )
