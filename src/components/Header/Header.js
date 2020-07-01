7edf3e45eab8f988a0af1b8f419560b05e161d35
Bartek-str <bartlomiejstruzyk@gmail.com>
2020-07-01 21:44:14 +0200

    done mobile view

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./../../assets/lisc_kolor.svg";
-import Burger from "./Burger";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faShoppingBag, faUserCircle } from "@fortawesome/free-solid-svg-icons";
+// use react-scroll later
+// do animations to moveIn and moveOut menu
 
-const Header = () => (
-    <div className='Header'>
-        <div className='logo'>
-            <div className='api'>API</div>
-            <div className='container'>
-                <img src={Logo} alt='lisc'/>
-                <div className='drogeria'>drogeria</div>
+const Header = () => {
+    const [active, setActive] = useState(false);
+
+    return (
+        <div className='Header'>
+            {active ? <div className='shadow' /> : ''}
+            <div className={active ? 'nav-links x' : 'nav-links'}>
+                <div>Sklep</div>
+                <div>O nas</div>
+                <div>Kontakt</div>
+            </div>
+            <div className='logo'>
+                <div className='api'>API</div>
+                <div className='container'>
+                    <img src={Logo} alt='lisc'/>
+                    <div className='drogeria'>drogeria</div>
+                </div>
+            </div>
+            <div className={active ? 'nav-shop x' : 'nav-shop'}>
+                <FontAwesomeIcon icon={faSearch} className='icon' />
+                <FontAwesomeIcon icon={faUserCircle} className='icon'/>
+                <FontAwesomeIcon icon={faShoppingBag} className='icon' />
+            </div>
+            <div className={active ? 'burger x' : 'burger'} onClick={() => setActive(!active)}>
+                <div className='line1'/>
+                <div className='line2'/>
+                <div className='line3'/>
             </div>
         </div>
-        <Burger />
-    </div>
-)
+    )
+}
 
 export default Header;
